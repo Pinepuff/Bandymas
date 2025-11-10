1d124adea39abf4bc3fb640336697fd1ece22f8c
JustinaSab <84080737+JustinaSab@users.noreply.github.com>
2025-11-10 20:03:08 +0200

    011

---
+++ 011/app.js
@@ -0,0 +1,47 @@
+console.log("Labas Bebriau");
+
+var x = 10;
+
+console.log(x)
+
+var labasBarsukui = "Sveikas Barsuk";
+
+console.log(labasBarsukui);
+
+x = 20;
+
+console.log(x, "labasBarsukui");
+
+let K1 = 25; // tipas Numeris
+let K3 = '102';
+let K2 = 'Barsukas';
+
+let K20 = K1 + 30;
+console.log(K20);
+
+let K30 = 100 + 20 * 5;
+console.log(K30);
+
+let K40 = K1 + K3; //sulipdymas
+console.log(K40);
+
+let K50 = K2 + ' ' + K2;
+console.log(K50);
+
+let bebras = {
+    vardas: 'Justina',
+    amzius: 23
+}
+
+bebras.amzius = 14;
+bebras.uodega = 'Pusilge';
+
+console.log(bebras);
+console.log(bebras.amzius);
+
+let color = 'blue';
+let like = 'sky';
+
+let bodyColor = like + color;
+
+b.style.background = bodyColor;
